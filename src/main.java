2f93676baea8a7a0dabbed923b193fac9401afe4
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2018-09-20 10:50:35 +0200

    Create src/main.java

---
+++ src/main.java
@@ -0,0 +1,18 @@
+public class car {
+
+
+    public class Main {
+
+        public static void main(String[] args) {
+            for(int x=0;x<5;i++)
+            {
+                System.out.println(x);
+
+            }
+
+
+
+        }
+    }
+
+}
